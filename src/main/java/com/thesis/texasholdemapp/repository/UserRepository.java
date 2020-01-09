606a5779b913dbbb18b298c68e58b80e7b8fabe2
Sebastian Szychulski <sn.szychulski@gmail.com>
2020-01-09 16:03:45 +0100

    Before refractoring

--- src/main/java/com/thesis/texasholdemapp/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.thesis.texasholdemapp.repository;
-
-import com.thesis.texasholdemapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
--- src/main/java/com/thesis/texasholdemapp/service/SecurityService.java
+++
@@ -1,6 +0,0 @@
-package com.thesis.texasholdemapp.service;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-    void autoLogin(String username, String password);
-}
